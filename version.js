ebc2c781619a2593001ee9bec76edf6e7e1a09a4
facturacionWeb <webappfacturacion@gmail.com>
2024-10-19 15:00:59 -0600

    Create version.js

---
+++ version.js
@@ -0,0 +1,7 @@
+// version.js
+document.addEventListener("DOMContentLoaded", function() {
+    const versionContainer = document.createElement("div");
+    versionContainer.classList.add("container");
+    versionContainer.innerHTML = '<p>Versión del sitio: 2024101910</p>';
+    document.body.appendChild(versionContainer);
+});

1166c8963caa4d11c748a73007bf727e7165b0b6
facturacionWeb <webappfacturacion@gmail.com>
2024-10-19 23:01:58 -0600

    Update version.js

--- version.js
+++ version.js
@@ -2,7 +2,7 @@
 (function() {
     const versionContainer = document.createElement("div");
     versionContainer.classList.add("container");
-    versionContainer.innerHTML = '<p>Versión del sitio: 2024101925</p>';
+    versionContainer.innerHTML = '<p>Versión del sitio: 2024101926</p>';
     document.body.appendChild(versionContainer);
 })();
 

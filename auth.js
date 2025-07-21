29a66311318b610acc396d119316c25e6f4c0e7e
facturacionWeb <webappfacturacion@gmail.com>
2025-07-21 13:09:59 -0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,11 @@
 export function requireAccess(pageKey){
+
+  console.log("👤 Usuario cargado:", u);
+  console.log("🔑 Página requerida:", pageKey);
+  console.log("📄 Páginas permitidas:", u.allowedPages);
+  console.log("✔ ¿Incluye la página?", u.allowedPages.includes(pageKey));
+
+  
   const u = JSON.parse(sessionStorage.getItem('currentUser') || 'null');
   if(!u){alert('Debe iniciar sesión');return redirect();}
   if(!u.allowedPages.includes(pageKey)){alert('Sin permiso');return redirect();}
